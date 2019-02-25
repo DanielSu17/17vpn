c65f4e722c419e8fb934ecf36a43ff2a21927695
guessi <guessi@gmail.com>
2019-02-25 18:53:29 +0800

    [Ops] Say Hello to Python 3 (#123)
    * [Ops] Say Hello to Python 3
    
    Fix deprecation warning:
    
    ```
    DEPRECATION: Python 2.7 will reach the end of its life on January 1st, 2020.
    Please upgrade your Python as Python 2.7 won't be maintained after that date.
    A future version of pip will drop support for Python 2.7.
    ```
    
    Time to upgrade to Python 3 :-)
    
    * [Ops] Hello Python 3 - Fix file extension check
    
    * [Ops] Hello Python 3 - Fix file extension check

--- circle/syntax_checker.py
+++ circle/syntax_checker.py
@@ -1,34 +1,41 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import os
-import yaml
 import json
-from ruamel.yaml import YAML
+import os
+import platform
 import sys
+import yaml
+
+from ruamel.yaml import YAML
+
+print("Python Runtime Version: {0}".format(platform.python_version()))
 
 check_file_count = 0
 sytax_failed_count = 0
 for dirpath, dirnames, filenames in os.walk("."):
-    for filename in [f for f in filenames if f.endswith(('.yaml', '.json', 'yml'))]:
+    filenames = [f for f in filenames if f.endswith(('.yaml', '.json', '.yml'))]
+    for filename in filenames:
         check_file_count += 1
         full_filename = os.path.join(dirpath, filename)
         with open(full_filename, 'r') as stream:
             try:
-                if full_filename.endswith('.yaml') or full_filename.endswith('yml'):
-                    # YAML(typ='safe') accomplishes the same as what yaml.safe_load() did
+                if (full_filename.endswith('.yaml') or
+                    full_filename.endswith('.yml')):
                     yamll=YAML(typ='safe')
                     yamll.load(stream)
                 if full_filename.endswith('.json'):
                     json.load(stream)
             except yaml.YAMLError as exc:
-                print '%s => %s' % (full_filename, exc)
                 sytax_failed_count += 1
+                print('{0} => {1}'.format(full_filename, exc))
             except ValueError as exc:
-                print '%s => %s' % (full_filename, exc)
                 sytax_failed_count += 1
+                print('{0} => {1}'.format(full_filename, exc))
             finally:
                 stream.close()
-print 'Need Syntax Check Files Count: %s, Failed Files Count: %s' % (check_file_count, sytax_failed_count)
+
+print('Checked {0} files, Failed: {1}'.format(
+    check_file_count, sytax_failed_count))
 
 if sytax_failed_count > 0:
     sys.exit(1)
