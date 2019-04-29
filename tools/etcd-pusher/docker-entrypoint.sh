3a755c6cdb1966a732aed8881158abb98de8fe8a
guessi <guessi@gmail.com>
2019-04-29 19:03:34 +0800

    Fix etcd update failure

--- tools/etcd-pusher/docker-entrypoint.sh
+++ tools/etcd-pusher/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yes yes | /opt/pusher                                                \
-  -csi.config.repo.root /configs/envs/${ENVIRONMENT}/${APPLICATION}  \
-  -csi.config.etcd.root /configs/envs/${ENVIRONMENT}/${APPLICATION}  \
+yes yes | /opt/pusher                                \
+  -csi.config.repo.root /configs/envs/${ENVIRONMENT} \
+  -csi.config.etcd.root /configs/envs/${ENVIRONMENT} \
   -csi.config.etcd.machines ${ENDPOINTS}
