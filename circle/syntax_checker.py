155b60057903ffab256016b3d9f816fa73096fce
guessi <guessi@gmail.com>
2019-08-26 13:59:45 +0800

    [Ops] Check invisible characters in YAML configs (#1180)

--- circle/syntax_checker.py
+++ circle/syntax_checker.py
@@ -10,8 +10,18 @@ from ruamel.yaml import YAML
 
 print("Python Runtime Version: {0}".format(platform.python_version()))
 
+invalid_chars = [
+    u'\u200a', # Hair Space
+    u'\u200b', # Zero-Width Space
+    u'\u200c', # Zero Width Non-Joiner
+    u'\u200d', # Zero Width Joiner
+    u'\u200e', # Left-To-Right Mark
+    u'\u200f', # Right-To-Left Mark
+]
+
 check_file_count = 0
 sytax_failed_count = 0
+
 for dirpath, dirnames, filenames in os.walk("."):
     filenames = [f for f in filenames if f.endswith(('.yaml', '.json', '.yml'))]
     for filename in filenames:
@@ -19,18 +29,34 @@ for dirpath, dirnames, filenames in os.walk("."):
         full_filename = os.path.join(dirpath, filename)
         with open(full_filename, 'r') as stream:
             try:
-                if (full_filename.endswith('.yaml') or
-                    full_filename.endswith('.yml')):
+                if full_filename.endswith(('.yaml', '.yml')):
+                    # check invisible characters
+                    line_number = 0
+                    for line in stream.readlines():
+                        line_number += 1
+                        for char in invalid_chars:
+                            if char in line:
+                                print('{0}: invisible character found at line {1}'
+                                        .format(full_filename, line_number))
+                                # TODO: should return error after stable
+                                # sys.exit(1)
+
+                    # check yaml format
                     yamll=YAML(typ='safe')
                     yamll.load(stream)
+
                 if full_filename.endswith('.json'):
+                    # check json format
                     json.load(stream)
+
             except yaml.YAMLError as exc:
                 sytax_failed_count += 1
                 print('{0} => {1}'.format(full_filename, exc))
             except ValueError as exc:
                 sytax_failed_count += 1
                 print('{0} => {1}'.format(full_filename, exc))
+            except:
+                print("Oops... something went wrong")
             finally:
                 stream.close()
 
