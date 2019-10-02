3281905bd91bf7fe8153423be7dd896d9c8e2a62
guessi <guessi@gmail.com>
2019-10-03 00:31:35 +0800

    [Ops] Improve check-duplicated-users scripts (#1425)

--- tools/check_duplicate_users.py
+++ tools/check_duplicate_users.py
@@ -10,31 +10,39 @@ yaml=YAML(typ="safe")
 
 print("Python Runtime Version: {0}".format(platform.python_version()))
 
-target_file = "envs/prod/17app/stream/providers.yaml"
+def check_duplicate(input_file):
+    print("Checking: {0}".format(input_file))
 
-with open(target_file, 'r') as f:
-    data = yaml.load(f)
+    with open(input_file, 'r') as f:
+        data = yaml.load(f)
 
-# construct dict of users
-users_dict = {}
-for provider_config in data['rtmp_providers']:
-    # skip if no 'name' defined
-    if 'name' not in provider_config.keys():
-        continue
-    name = provider_config['name']
-    # skip if no 'users' defined
-    if 'users' in provider_config.keys():
-        users_dict[name] = provider_config['users']
+    # construct dict of users
+    users_dict = {}
+    for provider_config in data['rtmp_providers']:
+        # skip if no 'name' defined
+        if 'name' not in provider_config.keys():
+            continue
+        name = provider_config['name']
+        # skip if no 'users' defined
+        if 'users' in provider_config.keys():
+            users_dict[name] = provider_config['users']
 
-unique_list = []
-duplicate_user_count = 0
-for name, users in users_dict.items():
-    for user in users:
-        if user in unique_list:
-            print("Warning: duplicate user in {} list: {}".format(name, user))
-            duplicate_user_count += 1
-        else:
-            unique_list.append(user)
+    unique_list = []
+    duplicate_user_count = 0
+    for name, users in users_dict.items():
+        for user in users:
+            if user in unique_list:
+                print("* Warning: duplicate user in [{0}: {1}]: {2}".format(input_file, name, user))
+                duplicate_user_count += 1
+            else:
+                unique_list.append(user)
 
-if duplicate_user_count > 0:
-    sys.exit(1)
+    if duplicate_user_count > 0:
+        print("[FAIL]")
+        sys.exit(1)
+
+    print("[PASS]")
+
+for env in ["dev", "sta", "prod"]:
+    target_file = "envs/" + env + "/17app/stream/providers.yaml"
+    check_duplicate(target_file)
