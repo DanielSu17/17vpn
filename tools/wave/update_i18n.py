cdc0b29f9b0dd2efde2073e266b9333e9c4ba160
carlcarl <carlcarlking@gmail.com>
2020-04-21 17:59:03 +0800

    [Wave] Add update_i18n for wave (#3068)
    * [Wave] Add update_i18n for wave
    
    * Update

---
+++ tools/wave/update_i18n.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+
+import io
+import os
+import re
+import sys
+import json
+import time
+import requests
+import argparse
+
+ENVS = ["dev", "sta", "prod"]
+
+backend_internal_mapping = {
+    "zh_TW": "zh-hant",
+    "zh_CN": "zh-hans",
+    "en":    "en",
+    "zh_SG": "zh_SG",
+}
+
+class I18nJsonWriter:
+
+    # json pretty
+    indent = 2
+    sort_keys = True
+    separators=(',', ': ')
+
+    # file related
+    path = "../../envs/%s/%s/i18n/%s/%s.json"
+
+    def __init__(self, i18n_dict, project):
+        self.i18n_dict = i18n_dict
+        self.project = project
+
+    def prepare_data(self, data, param_prefix):
+        """
+        @type data: dict
+        @type param_prefix: regex
+        @rtype: string
+        """
+        data_str = json.dumps(data, indent=self.indent, sort_keys=self.sort_keys, separators=self.separators, ensure_ascii=False).encode('utf8')
+
+        if param_prefix is not None:
+            # We need to escape '%' or client may crash
+            data_str = re.sub(r'%', r'%%', data_str)
+            data_str = re.sub(r'\$([0-9]+)', param_prefix, data_str)
+
+        return data_str
+
+    def write_file(self, full_path, data):
+        """
+        write file
+
+        @type full_path: string
+        @type data: string
+        """
+        with open(full_path, 'w') as fh:
+            fh.write(data)
+
+    def write_data(self, envs, base_name, param_prefix=None):
+        """
+        @type base_name: string
+        @type param_prefix: regex
+        """
+        for env in envs:
+            for lang, value in self.i18n_dict.iteritems():
+                data_str = self.prepare_data(value, param_prefix)
+                full_path = self.path % (env, self.project, lang.lower(), base_name)
+
+                self.write_file(full_path, data_str)
+
+
+class LokaliseClient:
+    api_url = "https://api.lokalise.co/api/"
+
+    def __init__(self, api_token):
+        self.api_token = api_token
+
+    def get_project_id_by_name(self, project_name):
+        url = self.api_url + "project/list"
+        payload = {"api_token": self.api_token}
+        r = requests.get(url, params=payload)
+        if r.status_code != 200:
+            raise Exception("get languages failed")
+
+        return [i['id'] for i in r.json()['projects'] if project_name == i['name']][0]
+
+    def get_languages(self, project_id):
+        """
+        Return translated language of project id
+        """
+        url = self.api_url + "language/list"
+        payload = {
+            "api_token": self.api_token,
+            "id": project_id
+        }
+        r = requests.get(url, params=payload)
+        if r.status_code != 200:
+            raise Exception("get languages failed")
+
+        return [i['iso'] for i in r.json()['languages']]
+
+    def get_strings(self, project_id, langs):
+        """
+        Get strings
+
+        @return: {
+            "en": {
+                "key": "value"
+            },
+            "tw": {
+                "key": "value"
+            }
+        }
+        """
+        ret = dict()
+        url = self.api_url + "string/list"
+        data = {
+            "api_token": self.api_token,
+            "id": project_id,
+            "langs": langs
+        }
+        r = requests.post(url, data=data)
+        if r.status_code != 200:
+            raise Exception("get strings failed")
+        r_json = r.json()
+        if r_json['response']['code'] != '200':
+            raise Exception(r_json['response']['message'])
+
+        for lang, strings in r_json['strings'].iteritems():
+            ret[lang] = dict()
+            for string in strings:
+                ret[lang][string['key']] = string['translation']
+
+        return ret
+
+    def get_all_strings(self, project_id):
+        return self.get_strings(project_id, [])
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Pull i18n from Lokalise.')
+    parser.add_argument('env', choices=ENVS+["all"], default=ENVS, help='')
+    args = parser.parse_args()
+
+    if args.env == "all":
+        env = ENVS
+    else:
+        env = [args.env]
+
+    # get LOKALIZE_TOKEN from environment variable
+    LOKALIZE_TOKEN = os.environ['LOKALIZE_TOKEN']
+    if len(LOKALIZE_TOKEN.strip()) <= 0:
+        print('FATAL: undefined LOKALIZE_TOKEN')
+        sys.exit(1)
+
+    lc = LokaliseClient(LOKALIZE_TOKEN)
+
+    p_id = lc.get_project_id_by_name("Wave")
+    backend_client_keys = lc.get_all_strings(p_id)
+
+    for k, v in backend_internal_mapping.iteritems():
+      try:
+        backend_client_keys[v] = backend_client_keys.pop(k)
+      except KeyError:
+        continue
+
+    iw = I18nJsonWriter(backend_client_keys, "wave")
+    # Write ios.json and android.json
+    iw.write_data(env, "ios", param_prefix=r'%\1$@')
+    iw.write_data(env, "android", param_prefix=r'%\1$s')
+
+    time.sleep(5)
