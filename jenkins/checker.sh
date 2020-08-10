62d1ca4bc2070b844430293104cd6718cd55f3e7
KH Hsu <kevinhsu@17.media>
2020-08-10 15:27:16 +0800

    [Ops] Replace CircleCI with our Jenkins (#4201)
    * commit script
    
    * update
    
    * update

---
+++ jenkins/checker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+REQUIREMENT="./circle/requirements.txt"
+CHECKER="circle/syntax_checker.py circle/check_providers.py circle/remote_check.py"
+ERROR_TAGGING=',{"type":"section","text":{"type":"mrkdwn","text":"@sre"}}'
+COMMITER_INFO=`git log  --pretty=format:'%au (%ae)' ${GIT_COMMIT}^! `
+COMMIT_MSG=`git log  --pretty=format:'%B' ${GIT_COMMIT}^! `
+
+main(){
+    echo "--- start checking ---"
+    curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$COMMIT_URL | $GIT_COMMIT>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Build URL:*\n*<${BUILD_URL}|URL>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Status:*\nTest Started\n*Commited By:*\n ${COMMITER_INFO}\n*Commit Message:*\n${COMMIT_MSG} \"}}]}" ${SLACK}
+    for SCRIPT in ${CHECKER}; do
+        docker run --rm -v $(pwd):/repo tig4605246/config-checker-python:latest cd /repo && python3 /repo/${SCRIPT}
+        ## get status ##
+        STATUS=$?
+        ## take some decision ## 
+        if [ ${STATUS} -eq 0 ] 
+        then
+            echo "${SCRIPT} command was successful" 
+        else
+            curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$COMMIT_URL | $GIT_COMMIT>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Build URL:*\n*<${BUILD_URL}|URL>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Status:*\nTests Failed\n*Commited By:*\n ${COMMITER_INFO}\n*Commit Message:*\n${COMMIT_MSG} \"}}${ERROR_TAGGING}]}" ${SLACK}
+            exit -1
+        fi
+    done;
+    curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$COMMIT_URL | $GIT_COMMIT>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Build URL:*\n*<${BUILD_URL}|URL>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Status:*\nTests Passed\n*Commited By:*\n ${COMMITER_INFO}\n*Commit Message:*\n${COMMIT_MSG} \"}}]}" ${SLACK} 
+    echo "--- finish checking ---"
+}
+
+main $@
