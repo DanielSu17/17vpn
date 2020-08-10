6647a47d117e20644a1918277adce5c14a291d90
KH Hsu <kevinhsu@17.media>
2020-08-10 18:46:15 +0800

    [Ops] Fix Checker Typo (#4213)
    * update checker.sh
    
    * fix commiter name
    
    * error test
    
    * better error msg
    
    * bold error
    
    * lower case and revert config error

--- jenkins/checker.sh
+++ jenkins/checker.sh
@@ -3,14 +3,14 @@
 REQUIREMENT="./circle/requirements.txt"
 CHECKER="circle/syntax_checker.py circle/check_providers.py circle/remote_check.py"
 ERROR_TAGGING=',{"type":"section","text":{"type":"mrkdwn","text":"@sre"}}'
-COMMITER_INFO=`git log  --pretty=format:'%au (%ae)' ${GIT_COMMIT}^! `
+COMMITER_INFO=`git log  --pretty=format:'%an (%ae)' ${GIT_COMMIT}^! `
 COMMIT_MSG=`git log  --pretty=format:'%B' ${GIT_COMMIT}^! `
 
 main(){
     echo "--- start checking ---"
     curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$COMMIT_URL | $GIT_COMMIT>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Build URL:*\n*<${BUILD_URL}|URL>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Status:*\nTest Started\n*Commited By:*\n ${COMMITER_INFO}\n*Commit Message:*\n${COMMIT_MSG} \"}}]}" ${SLACK}
     for SCRIPT in ${CHECKER}; do
-        docker run --rm -v $(pwd):/repo tig4605246/config-checker-python:latest /bin/sh -c "/repo && python3 /repo/${SCRIPT}"
+        docker run --rm -v $(pwd):/repo tig4605246/config-checker-python:latest /bin/sh -c "cd /repo && python3 /repo/${SCRIPT}"
         ## get status ##
         STATUS=$?
         ## take some decision ## 
@@ -18,7 +18,7 @@ main(){
         then
             echo "${SCRIPT} command was successful" 
         else
-            curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$COMMIT_URL | $GIT_COMMIT>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Build URL:*\n*<${BUILD_URL}|URL>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Status:*\nTests Failed\n*Commited By:*\n ${COMMITER_INFO}\n*Commit Message:*\n${COMMIT_MSG} \"}}${ERROR_TAGGING}]}" ${SLACK}
+            curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"Config commit:\n*<https://github.com/tig4605246/actionary/commit/$COMMIT_URL | $GIT_COMMIT>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Build URL:*\n*<${BUILD_URL}|URL>*\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"*Status:*\nTests of *${SCRIPT}* failed\n*Commited By:*\n ${COMMITER_INFO}\n*Commit Message:*\n${COMMIT_MSG} \"}}${ERROR_TAGGING}]}" ${SLACK}
             exit -1
         fi
     done;
