e99322e88c174d2ca218f19c7b50626267dd25c3
kuoandy <andykuo@17.media>
2020-12-30 18:29:18 +0800

    remove Backend_Internal logic in update_i18n.py (#5184)
    1. 主要是改 update_i18n.py
    2. 其他 files 是跑完 script 的結果
    3. 之前過渡期需要 backend_internal keys 的也在 i18n 翻譯包裡，過渡期現在已經過了，可以移除掉

--- tools/update_i18n.py
+++ tools/update_i18n.py
@@ -159,51 +159,6 @@ class LokaliseClient:
             page+=1
         return ret
 
-def merge_i18nkeys(i18nkeys1, i18nkeys2):
-    merged_keys = {}
-    for language, keys in i18nkeys2.iteritems():
-        if language not in i18nkeys1:
-            merged_keys[language] = keys
-            continue
-
-        temp = keys
-        temp.update(i18nkeys1[language])
-        merged_keys[language] = temp
-    return merged_keys
-
-
-def duplicate_key_exist(i18nkeys1, i18nkeys2):
-    # because all the key names under each language are the same in same project,
-    # we check "tw" between two projects' keys is ok enough
-    # ex: 17 backend:
-    # {
-    #     "en": {
-    #         "key1",
-    #         "key2"
-    #     },
-    #     "tw": {
-    #         "key1",
-    #         "key2"
-    #     }
-    # }
-
-    # ex: 17 backend.internal:
-    # {
-    #     "en": {
-    #         "key3",
-    #         "key4"
-    #     },
-    #     "tw": {
-    #         "key3",
-    #         "key4"
-    #     }
-    # }
-
-    keys1 = set(i18nkeys1["tw"].keys())
-    keys2 = set(i18nkeys2["tw"].keys())
-    return keys1 & keys2
-
-
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Pull i18n from Lokalise.')
     parser.add_argument('env', choices=ENVS+["all"], default=ENVS, help='')
@@ -221,42 +176,17 @@ if __name__=="__main__":
         sys.exit(1)
 
     lc = LokaliseClient(LOKALIZE_TOKEN)
-
-    # get i18n keys from backend internal
-    p_id = lc.get_project_id_by_name("17.backend.internal")
-    backend_internal_keys = lc.get_translations(p_id)
-    # change backend.internal's language value to the one matching 17.backend and 17.backend(client)
-    for k, v in backend_internal_mapping.iteritems():
-      try:
-        backend_internal_keys[v] = backend_internal_keys.pop(k)
-      except KeyError:
-        continue
-
-    # get_all_strings This method allows one request per 5 seconds
-    time.sleep(5)
     
-    # check if duplicate keys exist in 17.backend.internal and 17.backend(client)
+    # Write to ios.json and android.json based on "17.backend(client)"
     p_id = lc.get_project_id_by_name("17.backend(client)")
-    backend_client_keys = lc.get_translations(p_id)
-    duplicate_keys = list(duplicate_key_exist(backend_internal_keys, backend_client_keys))
-    if len(duplicate_keys) > 0:
-        sys.stderr.write("duplicate keys are found in 17.backend.internal and 17.backend(client)\n%s" %('\n'.join(duplicate_keys)))
-        sys.exit(1)
-
-    # merge 17.backend.internal and 17.backend(client)
-    backend_client_keys = merge_i18nkeys(backend_internal_keys, backend_client_keys)
-    iw = I18nJsonWriter(backend_client_keys, "17app")
-    # Write ios.json and android.json
+    iw = I18nJsonWriter(lc.get_translations(p_id), "17app")
     iw.write_data(env, "ios", param_prefix=r'%\1$@')
     iw.write_data(env, "android", param_prefix=r'%\1$s')
 
     # get_all_strings This method allows one request per 5 seconds
     time.sleep(5)
 
-    # Write backend.json
+    # Write to backend.json based on 17.backend
     p_id = lc.get_project_id_by_name("17.backend")
     iw = I18nJsonWriter(lc.get_translations(p_id), "17app")
     iw.write_data(env, "backend")
-
-    # get_all_strings This method allows one request per 5 seconds
-    time.sleep(5)
