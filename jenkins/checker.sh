9e719793f79dbdd93a27db0f1afcc22415474182
KH Hsu <kevinhsu@17.media>
2021-01-12 16:35:48 +0800

    [Ops] Support mutiple ENV in one Push (#5241)
    * edit push
    
    * update checker
    
    * uncomment push
    
    * update
    
    * update message
    
    * update msg
    
    * update msg
    
    * format commits

--- jenkins/checker.sh
+++ jenkins/checker.sh
@@ -2,9 +2,16 @@
 
 CHECKER="circle/syntax_checker.py circle/check_providers.py circle/remote_check.py"
 ERROR_TAGGING=',{"type":"section","text":{"type":"mrkdwn","text":"@sre"}}'
-COMMIT_MESSAGE=$(git log  --pretty=format:'%B' "${GIT_COMMIT}"^! | head -n1)
+#COMMIT_MESSAGE=$(git log  --pretty=format:'%B' "${GIT_COMMIT}"^! | head -n1)
+commits=$(git log  --pretty=format:'%H' "${GIT_PREVIOUS_SUCCESSFUL_COMMIT}".."${GIT_COMMIT}")
+
+for commit in ${commits};do
+    message=$(git log  --pretty=format:'%B' "${commit}"^! | head -n1)
+    COMMIT_MESSAGE=$(printf "%s\n%s" "${COMMIT_MESSAGE}" "${message}")
+done
 
 TMP=$(git log  --pretty=format:'%an (%ae)' "${GIT_COMMIT}"^!)
+
 # If Author is empty, it'll fail
 if [[ "${TMP}" == *"17.media"* ]]; then
     COMMITER_INFO=$(git log  --pretty=format:'%an (%ae)' "${GIT_COMMIT}"^! | cut -d'(' -f 2 | cut -d')' -f 1)
@@ -22,9 +29,10 @@ main(){
     echo "--- start checking ---"
     HEADER="Authorization: Bearer ${SLACKTOKEN}"
     USERID=$(curl -s -X GET --header "${HEADER}" https://slack.com/api/users.lookupByEmail?email="${COMMITER_INFO}" | jq -r .user.id)
-    curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"plain_text\",\"text\":\":crossed_fingers: Check Started\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"mrkdwn\",\"text\":\"*Author*:${COMMITER_INFO}  *Commit*:<https://github.com/17media/configs/commit/${GIT_COMMIT} | ${GIT_COMMIT}>\n*Build*:<${BUILD_URL}|URL>  *Message*:${COMMIT_MESSAGE}\"}]},{\"type\":\"divider\"}]}" "${SLACK}"
+    curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"plain_text\",\"text\":\":crossed_fingers: Check Started\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"mrkdwn\",\"text\":\"*Author*:${COMMITER_INFO}  *Commit*:<https://github.com/17media/configs/commit/${GIT_COMMIT} | ${GIT_COMMIT}> *Build*:<${BUILD_URL}|URL> \"}]},{\"type\":\"divider\"}]}" "${SLACK}"
     for SCRIPT in ${CHECKER}; do
         docker run --rm -v "$(pwd)":/repo 17media/config-check:latest /bin/sh -c "cd /repo && python3 /repo/${SCRIPT}"
+        
         ## get output status ##
         STATUS=$?
         ## take some decision ##
@@ -37,7 +45,7 @@ main(){
             exit 1
         fi
     done;
-    curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"plain_text\",\"text\":\":white_check_mark: Check Passed\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"mrkdwn\",\"text\":\"*Author*:${COMMITER_INFO}  *Commit*:<https://github.com/17media/configs/commit/${GIT_COMMIT} | ${GIT_COMMIT}>\n*Build*:<${BUILD_URL}|URL>  *Message*:${COMMIT_MESSAGE}\"}]},{\"type\":\"divider\"}]}" "${SLACK}"
+    curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"plain_text\",\"text\":\":white_check_mark: Check Passed\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"mrkdwn\",\"text\":\"*Author*:${COMMITER_INFO}  *Commit*:<https://github.com/17media/configs/commit/${GIT_COMMIT} | ${GIT_COMMIT}> *Build*:<${BUILD_URL}|URL>\"}]},{\"type\":\"divider\"}]}" "${SLACK}"
     echo "--- finish checking ---"
     # TODO: Add push to etcd here
 }
--- push_to_etcd.sh
+++ push_to_etcd.sh
@@ -9,7 +9,6 @@ if [ -z "${REVISION}" ]; then
   exit 1
 fi
 
-COMMIT_ID="${REVISION}"
 COMMIT_MESSAGE=$(git log  --pretty=format:'%B' "${GIT_COMMIT}"^! | head -n1)
 
 function push() {
@@ -21,19 +20,41 @@ function push() {
     -t 17media/pusher:v19.4.25
 }
 
-config_paths=$(git diff-tree --no-commit-id --name-only -r "${COMMIT_ID}" | grep -E -o '^envs\/(sta|prod|uat)/[^\/]+' | sort | uniq)
+# list of commits
+commits=$(git log  --pretty=format:'%H' "${GIT_PREVIOUS_SUCCESSFUL_COMMIT}".."${GIT_COMMIT}")
+config_paths=""
+commit_list=""
+echo "[debug] commits: ${commits}"
+
+# Get all change between lastest commit and previously successful commit
+for commit in ${commits}; do
+  echo "[debug] diff commit ${commit}"
+  tmp=$(git diff-tree --no-commit-id --name-only -r "${commit}" | grep -E -o '^envs\/(sta|prod|uat)/[^\/]+' ||true;)
+  if [ -n "${tmp}" ];then
+    config_paths=$(printf "%s %s" "${config_paths}" "${tmp}")
+    short_msg=$(git log  --pretty=format:'(%an) %B' "${commit}"^! | head -n1)
+    commit_list=$(printf "%s\n%s - %s" "${commit_list}" "${commit}" "${short_msg}")
+  fi
+done
+
+if [ "${config_paths}" == "" ]; then
+  echo "Nothing to push"
+  exit 0
+fi
+# merge same paths
+config_paths=$(printf "%s" "${config_paths}" | sort | uniq)
 echo "[debug] detected changes with config_paths: ${config_paths}"
+curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"plain_text\",\"text\":\":merged2: *Updating Following Config Changes*\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"mrkdwn\",\"text\":\"${commit_list}\"}]},{\"type\":\"divider\"}]}" "${SLACK}"    
 
-curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"plain_text\",\"text\":\":muscle:Push to ETCD Started.:etcd:\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"mrkdwn\",\"text\":\"*Message*:${COMMIT_MESSAGE}\n*Commit*:${GIT_COMMIT}  *Job*: <${BUILD_URL}|URL>\"}]},{\"type\":\"divider\"}]}" "${SLACK}"
-
-for config_path in $config_paths; do
+for config_path in ${config_paths}; do
   echo "[debug] processing config_path: ${config_path}"
 
   config_env=$(echo "${config_path}" | cut -d'/' -f2)
   config_app=$(echo "${config_path}" | cut -d'/' -f3)
 
   # support prod/sta only
-  if [ "${config_env}" = "sta" ] || [ "${config_env}" = "prod" ] || [ "${config_env}" = "uat" ]; then
+  if [ "${config_env}" = "sta" ]  || [ "${config_env}" = "prod" ] || [ "${config_env}" = "uat" ]; then
+    curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"plain_text\",\"text\":\":muscle:Push to ETCD Started. (${config_env}):etcd:\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"mrkdwn\",\"text\":\"*Message*:${COMMIT_MESSAGE}\n*Lastest Commit*:${GIT_COMMIT}  *Job*: <${BUILD_URL}|URL>\"}]},{\"type\":\"divider\"}]}" "${SLACK}"    
     # naming rule: ENDPOINTS_{{ APP_NAME }}_{{ CONFIG_ENV }}
     dynamic_endpoints="ENDPOINTS_$(echo "${config_app}" | tr '[:lower:]' '[:upper:]')_$(echo "${config_env}" | tr '[:lower:]' '[:upper:]')"
     endpoints=$(eval echo "\$${dynamic_endpoints}")
@@ -43,13 +64,12 @@ for config_path in $config_paths; do
       push "${config_env}" "${config_app}" "${endpoints}"
     else
       echo "abort, no endpoint defined"
-      curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"plain_text\",\"text\":\":warning:Push to ETCD Failed.:red_thinking::etcd:\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"mrkdwn\",\"text\":\"*Message*:${COMMIT_MESSAGE}\n*Commit*:${GIT_COMMIT}  *Job*: <${BUILD_URL}|URL> @sre\"}]},{\"type\":\"divider\"}]}" "${SLACK}"
+      curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"plain_text\",\"text\":\":warning:Push to ETCD Failed. (${config_env}):red_thinking::etcd:\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"mrkdwn\",\"text\":\"*Message*:${COMMIT_MESSAGE}\n*Lastest Commit*:${GIT_COMMIT}  *Job*: <${BUILD_URL}|URL> @sre\"}]},{\"type\":\"divider\"}]}" "${SLACK}"
       exit 1
     fi
+    curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"plain_text\",\"text\":\":white_check_mark:Push to ETCD Successfully. (${config_env}):etcd:\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"mrkdwn\",\"text\":\"*Message*:${COMMIT_MESSAGE}\n*Lastest Commit*:${GIT_COMMIT}  *Job*: <${BUILD_URL}|URL>\"}]},{\"type\":\"divider\"}]}" "${SLACK}"
   else
     echo "[debug] unsupported config_env, skipped" 
     continue
   fi
 done
-
-curl -X POST -H 'Content-type: application/json' --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"plain_text\",\"text\":\":white_check_mark:Push to ETCD Successfully.:etcd:\",\"emoji\":true}},{\"type\":\"context\",\"elements\":[{\"type\":\"mrkdwn\",\"text\":\"*Message*:${COMMIT_MESSAGE}\n*Commit*:${GIT_COMMIT}  *Job*: <${BUILD_URL}|URL>\"}]},{\"type\":\"divider\"}]}" "${SLACK}"
